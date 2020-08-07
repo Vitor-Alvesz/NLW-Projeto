7b6cd05e65bc4f5d59ab959b8f40d4ed8d59e062
Vitor Oliveira Rodrigues Alves <68556153+Vitor-Alvesz@users.noreply.github.com>
2020-08-07 00:41:42 -0300

    add gitbash server

---
+++ src/server.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const server = express()
+
+server.use(express.static("public"))
+.get("/",(req, res) => {
+    return res.sendFile(__dirname + "/view/index.html")
+} )
+.get("/study", (req, res) => {
+    return res.sendFile(__dirname + "/view/study.html")
+})
+.get("/give-classes", (req, res) => {
+    return res.sendFile(__dirname + "/view/give-classes.html")
+})
+
+.listen(5500)
