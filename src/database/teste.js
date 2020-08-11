901b96bfebbc9ea7e0a1a3fea0cd9674b5e57c9c
Vitor Oliveira Rodrigues Alves <68556153+Vitor-Alvesz@users.noreply.github.com>
2020-08-10 22:31:00 -0300

    end of the project

--- src/database/teste.js
+++ src/database/teste.js
@@ -14,7 +14,7 @@ Database.then(async (db) => {
     }
 
     classValue = {
-        subject: "Química", 
+        subject: "1", 
         cost:"20", 
         
     }
@@ -35,4 +35,29 @@ Database.then(async (db) => {
     //await createProffy(db, {proffyValue, classValue, classScheduleValues})
    
     //Consultar os dados inseridos
+
+    const selectedProffys = await db.all("SELECT * FROM proffys")
+
+
+    //consultar as classes de um determinado professor
+    //e trazer junto os dados do professor
+    const selectClassesAndProffys = await db.all(`
+        SELECT classes.*, proffys.*
+        FROM proffys
+        JOIN classes ON (classes.proffy_id = proffys.id)
+        WHERE classes.proffy_id = 1;
+    `)
+    
+
+    //o horario que a pessoa trabalha, por exemplo, é das 8h-18h
+    //o horário do time_from (8h) precisa ser menor ou igual o horário solicitado
+    // o time_to precisa ser acima
+    const selectclassesSchedules = await db.all(`
+        SELECT classes_schedule.*
+        FROM class_schedule
+        WHERE class_schedule.class_id = "1"
+        AND class_schedule.weekday = "0"
+        AND class_schedule.time_from <= "1300"
+        AND class_schedule.time_to > "1300"
+    `)
 })
